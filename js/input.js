674374fea739da4946fecbca96303b86c577ed7e
dennisd1 <mbl.acc4@gmail.com>
2016-12-23 23:59:12 +0300

    Update input.js

--- js/input.js
+++ js/input.js
@@ -4,9 +4,7 @@ var lastSelected;
 var select = document.querySelector(".timeinput");
 var dateselect = document.getElementById('inputdate');
 var nameselect = document.getElementById('inputname');
-
-var list = new Array(select, dateselect, timeselect);
-
+//
 var selectOption = select.options[select.selectedIndex];
 var lastSelected = localStorage.getItem('select');
 
@@ -19,16 +17,18 @@ select.onchange = function () {
    console.log(lastSelected);
    localStorage.setItem('select', lastSelected);
 }
-
-
-
+//
 dateselect.onchange = function () {
-   localStorage['inputdate3'] = document.getElementById('inputdate').value;
+   localStorage['inputdate'] = document.getElementById('inputdate').value;
 }
-    
- setInterval(function() {
-      localStorage['inputdate1'] = document.getElementById('inputdate').value;
-      localStorage['inputname2'] = document.getElementById('inputname').value;
- }, 20 * 1000);
+
+nameselect.onchange = function () {
+   localStorage['inputname'] = document.getElementById('inputname').value;
+}
+
+ //setInterval(function() {
+ //     localStorage['inputdate1'] = document.getElementById('inputdate').value;
+ //     localStorage['inputname2'] = document.getElementById('inputname').value;
+ //}, 20 * 1000);
 //var input = document.getElementById('dateinput').value;
 //localStorage.setItem('date', input);
