f06dd2b1d96a4cd61973171bb78c14ffd3258c6b
dennisd1 <mbl.acc4@gmail.com>
2016-12-23 21:23:06 +0300

    ec

---
+++ js/input.js
@@ -0,0 +1,17 @@
+<script>
+var lastSelected;
+
+var select = document.querySelector(".timeinput");
+var selectOption = select.options[select.selectedIndex];
+var lastSelected = localStorage.getItem('select');
+
+if(lastSelected) {
+    select.value = lastSelected; 
+}
+
+select.onchange = function () {
+   lastSelected = select.options[select.selectedIndex].value;
+   console.log(lastSelected);
+   localStorage.setItem('select', lastSelected);
+}
+</script>
